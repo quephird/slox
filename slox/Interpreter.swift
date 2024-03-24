4de1578f60c5f7a8ad205f1603f402700566ec66
quephird <danielle_kefford@hotmail.com>
2024-03-24 16:05:31 -0700

    OMFG THIS WORKS.

--- slox/Interpreter.swift
+++ slox/Interpreter.swift
@@ -17,6 +17,33 @@ class Interpreter {
             deleteAt(index) {
                 return deleteAtNative(this, index);
             }
+
+            map(fn) {
+                var result = [];
+                for (var i = 0; i < this.count; i = i + 1) {
+                    var newElement = fn(this[i]);
+                    result.append(newElement);
+                }
+                return result;
+            }
+
+            filter(fn) {
+                var result = [];
+                for (var i = 0; i < this.count; i = i + 1) {
+                    if (fn(this[i])) {
+                        result.append(this[i]);
+                    }
+                }
+                return result;
+            }
+
+            reduce(initial, fn) {
+                var result = initial;
+                for (var i = 0; i < this.count; i = i + 1) {
+                    result = fn(result, this[i]);
+                }
+                return result;
+            }
         }
 """
     var environment: Environment = Environment()
